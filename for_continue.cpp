66d12b8f2787bb62a9db2488f0399d8504240fc6
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-20 20:14:34 +0500

    Create for_continue.cpp

---
+++ for_continue.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        if (i == 3) continue;
+        cout << i << " ";
+    }
+    // Output: 1 2 4 5
+}
